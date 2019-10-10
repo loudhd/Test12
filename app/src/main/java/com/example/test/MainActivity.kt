a5b3ea531eabb253dba70e977f7832c862a29792
acer <wongsatorn1501003@gmail.com>
2019-10-10 22:53:52 +0700

    2

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dbHandler = DatabaseHandler(this)
+
+        button_save.setOnClickListener(View.OnClickListener {
+            // checking input text should not be null
+            if (validation()){
+                val user: User = User()
+                var success: Boolean = false
+                user.Name = editText_Name.text.toString()
+                user.Name = editText_Name.text.toString()
+
+                success = dbHandler!!.addUser(user)
+
+                if (success){
+                    val toast = Toast.makeText(this,"Saved Successfully", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
     }
 }
