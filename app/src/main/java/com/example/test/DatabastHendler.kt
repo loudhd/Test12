421c2db0f5f5572144fd5b971fc592e2d0c6933e
acer <wongsatorn1501003@gmail.com>
2019-10-10 22:42:13 +0700

    1

--- app/src/main/java/com/example/test/DatabastHendler.kt
+++ app/src/main/java/com/example/test/DatabastHendler.kt
@@ -1,8 +1,10 @@
 package com.example.test
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 import java.sql.SQLClientInfoException
 
 
@@ -13,6 +15,19 @@ class DatabaseHandler(context: Context) :
         val CREATE_TABLE = "CREATE TABLE $TABLE_NAME " +
                 "($ID Integer PRIMARY KEY, $FIRST_NAME TEXT, $LAST_NAME TEXT)"
         db?.execSQL(CREATE_TABLE)
-        
+    }
 
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        // Called when the database needs to be upgraded
+    }
+    fun addUser(user: User): Boolean {
+        //Create and/or open a database that will be used for reading and writing.
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(FIRST_NAME, user.firstName)
+        values.put(LAST_NAME, user.lastName)
+        val _success = db.insert(TABLE_NAME, null, values)
+        db.close()
+        Log.v("InsertedID", "$_success")
+        return (Integer.parseInt("$_success") != -1)
     }
--- app/src/main/java/com/example/test/User.kt
+++ app/src/main/java/com/example/test/User.kt
@@ -1,5 +1,8 @@
 package com.example.test
 
 class User {
-     
+    var id: Int = 0;
+    var fristname: String = "";
+    var lastname: String = "";
+
 }
