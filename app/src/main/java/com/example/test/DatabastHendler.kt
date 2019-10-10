5af8b5cfad9104a77e3c798b11ad380efe07b12b
acer <wongsatorn1501003@gmail.com>
2019-10-10 22:35:32 +0700

    4

---
+++ app/src/main/java/com/example/test/DatabastHendler.kt
@@ -0,0 +1,17 @@
+package com.example.test
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.sql.SQLClientInfoException
+
+
+class DatabaseHandler(context: Context) :
+    SQLiteOpenHelper(context, DB_NAME, null, DB_VERSIOM) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_TABLE = "CREATE TABLE $TABLE_NAME " +
+                "($ID Integer PRIMARY KEY, $FIRST_NAME TEXT, $LAST_NAME TEXT)"
+        db?.execSQL(CREATE_TABLE)
+        
+    }
